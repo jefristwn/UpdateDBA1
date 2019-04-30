d632954789022559f5589cc78a5669739e2d84c0
jefristwn <49868249+jefristwn@users.noreply.github.com>
2019-04-30 08:07:04 +0700

    Add files via upload

---
+++ A1 - Backup/src/Login/koneksi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jefri
+ */
+public class koneksi {
+    public static Connection Conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection konek = DriverManager.getConnection("jdbc:mysql://localhost/perusahaan1","root","");
+            return konek;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
